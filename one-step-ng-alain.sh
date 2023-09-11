fd523c3bd6c47ee44ff3ad5908f0541d45e04848
Yang <yangcjcjlu@126.com>
2023-09-11 14:45:04 +0800

    feat/one-step-script

---
+++ one-step-ng-alain.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Author:yangcj
+#Date:2023-09-11
+#Description: one step build up
+
+fold_path=./var/project
+project_path=ng-alain
+
+if ! [[ -d "$fold_path" ]]; then
+    mkdir -p $fold_path
+fi
+
+cd $fold_path
+
+if ! [[ -d "./$project_path" ]]; then
+    git clone --branch master --depth=1 https://github.com/Yangcjcjlu/ng-alain.git
+    cd $project_path
+else
+    cd $project_path
+    git pull origin master
+fi
+
+image_name="ng-alain-nginx"
+# container_id=$(docker ps -q -f "ancestor=$image_name")
+# if [[ -n "container_id" ]];
+#  then docker stop $container_id
+#  docker rmi
+
+docker build -t $image_name .
+docker run -d -p 4200:4200 $image_name
+
+echo "ng-alain started, please checked 4200!"
